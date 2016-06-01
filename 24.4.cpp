f540daf2d4edc98d669ed7fb9b71d998cc7519df
James Miles <jimdgmiles@gmail.com>
2016-06-01 07:28:20 +0100

    Almost finished

--- 24.4.cpp
+++ 24.4.cpp
@@ -3,21 +3,28 @@ using namespace std;
 
 int main()
 {
+srand (time(NULL));
 int n=100;
 int X[n];
-bool bad=1;
+bool done=1;
 
 for (int i=0; i<n; i++){
-  bad=1;
-  while(bad) {
+  done=0;
+  while(!done) {
     X[i]=rand()%n;
-    if(X[i]!=i){bad=0;}
+    if(X[i]!=i){done=1;}
   }
 }
 
 for (int i=0; i<n; i++){
+  if(X[X[X[i]]]==i){cout << i << " " << X[i] << " " << X[X[i]] << "\n";}
+}
+
+/*
+for (int i=0; i<n; i++){
   cout << i << ": " << X[i] << "\n";
 }
+*/
 
 return 0;
 }
