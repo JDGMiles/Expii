ae0e9c2b75b3bc88ad7d8359a586d52817d2957f
James Miles <jimdgmiles@gmail.com>
2016-06-01 08:11:22 +0100

    Finished

--- 24.4.cpp
+++ 24.4.cpp
@@ -5,9 +5,12 @@ int main()
 {
 srand (time(NULL));
 int n=100;
+int m=100000;
 int X[n];
 bool done=1;
+int count=0;
 
+for (int j=0; j<m; j++){
 for (int i=0; i<n; i++){
   done=0;
   while(!done) {
@@ -17,8 +20,11 @@ for (int i=0; i<n; i++){
 }
 
 for (int i=0; i<n; i++){
-  if(X[X[X[i]]]==i){cout << i << " " << X[i] << " " << X[X[i]] << "\n";}
+  if(X[X[X[i]]]==i){/*cout << i << " " << X[i] << " " << X[X[i]] << "\n";*/ i=n; count++;}
 }
+}
+
+cout << count << " out of " << m;
 
 /*
 for (int i=0; i<n; i++){
